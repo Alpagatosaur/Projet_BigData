44b6e7740cd27a0d33fbdd5f5ffe5f05f077b436
Alpagatosaur <tanch400@outlook.fr>
2021-04-05 16:12:37 +0200

    reprendre a 0

--- Codes/Get_csv.py
+++ Codes/Get_csv.py
@@ -7,9 +7,12 @@ Created on Sun Apr  4 10:10:02 2021
 
 import librosa
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import librosa.display
-
+from glob import glob
+import csv
+import pandas as pd
+import matplotlib as plt
 
 
 path = "Sons/chouette-hulotte-chant-et-cris5.wav"
@@ -20,10 +23,10 @@ n_fft = len(fft_y)
 
 melfb = librosa.filters.mel(sr, n_fft)
 
-fig, ax = plt.subplots()
+fig, ax = plt.pyplot.subplots()
 img = librosa.display.specshow(melfb, x_axis='linear', ax=ax)
 ax.set(ylabel='Mel filter', title='Mel filter bank')
-fig.colorbar(img, ax=ax)
+#fig.colorbar(img, ax=ax)
 
 
 
@@ -31,8 +34,7 @@ fig.colorbar(img, ax=ax)
 
 
 mfcc_y = librosa.feature.mfcc(y=y, sr=sr)
-
-fig, ax = plt.subplots(nrows=2, sharex=True, sharey=True)
+fig, ax = plt.pyplot.subplots(nrows=2, sharex=True, sharey=True)
 img1 = librosa.display.specshow(mfcc_y, x_axis='time', ax=ax[0])
 ax[0].set(title='MFCC')
 fig.colorbar(img1, ax=ax[0])
@@ -41,4 +43,14 @@ m_htk = librosa.feature.mfcc(y=y, sr=sr, dct_type=3)
 
 img2 = librosa.display.specshow(m_htk, x_axis='time', ax=ax[1])
 ax[1].set(title='HTK-style (dct_type=3)')
-fig.colorbar(img2, ax=[ax[1]])
+#fig.colorbar(img2, ax=[ax[1]])
+
+def creat(audio_files):
+    path = "Sons/" + "*.wav"
+    audio_files = glob(path) # dossier ou se trouve les sons ---a modifier si besoin
+    
+    for k in audio_files:
+        y,sr = librosa.load(path,sr=None)
+        fft_y =np.fft.fft(y)
+        n_fft = len(fft_y)
+        melfb = librosa.filters.mel(sr, n_fft)
