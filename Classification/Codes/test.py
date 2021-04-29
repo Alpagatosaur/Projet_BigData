66bd4723ad990d3a1cb1aeae322d79294ca9309b
Christine TAN <christine.tan@efrei.net>
2021-04-29 17:31:26 +0200

    Update test.py

--- Classification/Codes/test.py
+++ Classification/Codes/test.py
@@ -18,13 +18,3 @@ from sklearn.model_selection import train_test_split
 img_array = cv2.imread("Output/Autres/Autre1.png")
 bw_img = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
 
-ldata = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
-dfdata = pd.DataFrame(ldata, columns=['col1', 'col2', 'col3'], 
-		index=[0, 2, 3, 4])
-
-dfdata2 = dfdata.copy()
-mask = dfdata.col1 > 5
-dfdata2 = dfdata2[mask]
-dfdata2.col2 *= 2
-
-
