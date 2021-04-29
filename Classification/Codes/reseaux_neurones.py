2c9b74b15cfbbd6039729904d61f3c8d8a86ba77
Christine TAN <christine.tan@efrei.net>
2021-04-29 17:21:37 +0200

    V1.0.1

--- Classification/Codes/reseaux_neurones.py
+++ Classification/Codes/reseaux_neurones.py
@@ -24,18 +24,22 @@ for category in Classes:
     pathImg = Datadirectory + category
     pathImg = pathImg + "*.png"
     Img_files = glob(pathImg)
-    genre = category.split("/")
+    genre = 0  
     for img in Img_files:
         Ligne =[]
         img_array = cv2.imread(img)
-        print(img_array)
-        break
         new_array = cv2.resize(img_array,(img_size,img_size))
+        bw_img = cv2.cvtColor(new_array, cv2.COLOR_BGR2GRAY)
         if(len(new_array) !=0):
-            Ligne.append(new_array)
-            Ligne.append(genre[0])
+            Ligne.append(bw_img)
+            Ligne.append(genre)
             Data_List.append(Ligne)
+    genre = 1
+"""
+Genre = 0  == Autre
+Genre = 1 == Chouette ou hibou
 
+"""
 Data_List = shuffle(Data_List) # melange la liste
 train , test = train_test_split(Data_List)
 X_train=[]
@@ -45,30 +49,34 @@ for k in train:
     y_train.append(np.asarray(k[1]))
 
 #X_train = X_train/ 255
-
-input_shape = (216,3)
-
-"""
-np.shape(new_array)
-Out: (216, 216, 3)
-"""
+#X_train = tuple(X_train)
+X_train = np.asarray(X_train)
 
 
+y_train = np.asarray(y_train)
+
 """
-np.shape(Ligne[0][0])
-Out: (216, 3)
+X_train.shape
+Out: (252, 216, 216)
+
 """
+
+X_train = X_train.reshape(len(X_train),img_size*img_size)
+
+input_shape = (len(X_train),img_size*img_size)
+
+
 
 model = Sequential() # crée moi un réseau de neurones vides
 
-model.add(Dense(200, activation="relu", input_shape=input_shape )) #ajoute moi une couche de neurones (avec 100 neurones)
+model.add(Dense(200, activation="relu", input_shape=input_shape ))
 model.add(Dense(100, activation="relu"))
 model.add(Dense(10, activation="softmax"))
 
-sgd_optimizer = SGD(lr=3)
+sgd_optimizer = SGD(lr=2)
 
 model.compile(loss="binary_crossentropy", optimizer=sgd_optimizer, metrics=['accuracy'])
 
-model.fit(X_train, y_train, epochs=5, batch_size=32)
+model.fit(X_train, y_train, epochs=5)
 
 #print(model.score(X_train, y_train))
--- Classification/Codes/test.py
+++ Classification/Codes/test.py
@@ -5,4 +5,26 @@ Created on Wed Apr 28 18:22:26 2021
 @author: tanch
 """
 
-list=[[[1,2,3],2], [[5,2,3],4] , [[8,2,3],8] ]
+import cv2
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+
+img_array = cv2.imread("Output/Autres/Autre1.png")
+bw_img = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+
+ldata = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+dfdata = pd.DataFrame(ldata, columns=['col1', 'col2', 'col3'], 
+		index=[0, 2, 3, 4])
+
+dfdata2 = dfdata.copy()
+mask = dfdata.col1 > 5
+dfdata2 = dfdata2[mask]
+dfdata2.col2 *= 2
+
+
